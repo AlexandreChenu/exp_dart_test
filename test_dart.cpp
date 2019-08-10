f0ff77e1d40e38e95abb25c12e3d2bd80999fea6
Alexandre Chenu <38214810+AlexandreChenu@users.noreply.github.com>
2019-08-10 12:24:12 +0100

    changed nb_gen and l

--- test_dart.cpp
+++ test_dart.cpp
@@ -75,7 +75,7 @@ using namespace sferes::gen::dnn;
 struct Params {
     struct nov {
         SFERES_CONST size_t deep = 3;
-        SFERES_CONST double l = 0.7; // according to hand tuning made on the 2D arm simulation
+        SFERES_CONST double l = 0.3; // according to hand tuning made on the 2D arm simulation
         SFERES_CONST double k = 15; // TODO right value?
         SFERES_CONST double eps = 0.1;// TODO right value??
     };
@@ -86,7 +86,7 @@ struct Params {
         SFERES_CONST size_t init_size = 500;
         // size of a batch
         SFERES_CONST size_t size = 100;
-        SFERES_CONST size_t nb_gen = 10000;
+        SFERES_CONST size_t nb_gen = 10001;
         SFERES_CONST size_t dump_period = 500;
     };
 
